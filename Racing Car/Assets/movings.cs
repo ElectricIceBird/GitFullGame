0376edf512529cd3e2426bec7bd689d343fcf50e
ElectricIceBird <electricicebird23@gmail.com>
2021-01-16 10:09:02 +0500

    Project
    NewProjeeect

---
+++ Racing Car/Assets/movings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movings : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("up"))
+        {
+            rb.velocity = new Vector3(Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime,0,0);
+        }
+        if(Input.GetKeyDown("left"))
+        {
+            rb.AddForce(100,0,0);
+        }
+        if(Input.GetKeyDown("right"))
+        {
+            rb.AddForce(-100,0,0);
+        }
+        
+    }
+}
---
+++ Zombie Roller/Assets/Scripts/GameManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int ZombieSlectedPosition = 0;
+    public GameObject selectedZombie;
+    public List <GameObject> Zombie;
+    public Vector3 defaultScale;
+    public Vector3 SeleSize;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectedZombies (selectedZombie); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("left")){
+            GetZombieLeft();
+
+        }
+        if(Input.GetKeyDown("right")){
+             GetZombieRight();
+
+        }
+              if(Input.GetKeyDown("up")){
+                  PushUp();
+               
+
+              }
+
+
+    }
+    void GetZombieLeft()
+    {
+        if(ZombieSlectedPosition == 0)
+        {
+            ZombieSlectedPosition = 3;
+            SelectedZombies(Zombie[3]);
+        }else
+        {
+            ZombieSlectedPosition = ZombieSlectedPosition -1;
+            GameObject newZombie = Zombie [ZombieSlectedPosition];
+            SelectedZombies(newZombie);
+                       
+
+        }
+    }
+     void GetZombieRight()
+    {
+        if(ZombieSlectedPosition == 3)
+        {
+            ZombieSlectedPosition = 0;
+
+            SelectedZombies(Zombie[0]);
+        }else
+        {
+            ZombieSlectedPosition = ZombieSlectedPosition +1;
+
+            SelectedZombies(Zombie [ZombieSlectedPosition]);
+            
+                       
+
+        }
+    }
+    void SelectedZombies(GameObject newZombie)
+    { 
+        selectedZombie.transform.localScale = defaultScale;
+
+        selectedZombie = newZombie;
+        newZombie.transform.localScale = SeleSize;
+    }
+    void PushUp()
+    {
+        Rigidbody rb = selectedZombie.GetComponent<Rigidbody>();
+        rb.AddForce(0,0,10, ForceMode.Impulse);
+    }
+}
