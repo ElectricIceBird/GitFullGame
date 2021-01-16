70dac7b6af98d1d882eb85f615fd97af4b53b695
ElectricIceBird <electricicebird23@gmail.com>
2021-01-16 23:19:45 +0500

    Sumo

---
+++ Sumo/Assets/Sound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+   public  string strTag;
+    
+    public GameObject Particle;
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == strTag)
+            Instantiate(Particle,transform.position,transform.rotation);
+            
+    }
+}
