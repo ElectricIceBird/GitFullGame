656f4cecbcc8a694283c9c231697aa35e496d507
ElectricIceBird <electricicebird23@gmail.com>
2021-02-16 23:09:50 +0500

    Kiiled and side view start

---
+++ BrackeysJam2021Redone/Assets/Grounded.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            player.GetComponent<Move2D>().isGrounded = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            player.GetComponent<Move2D>().isGrounded = false;
+
+
+        }
+    }
+}
---
+++ BrackeysJam2021Redone/Assets/Move2D.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2D : MonoBehaviour
+{
+    public float moveSpeed;
+    public float Jumpfocre;
+    public bool isGrounded;
+    private float jumpCount;
+    public SpriteRenderer sr;
+    private float moveInput;
+    public int jumpExtravalue;
+    private bool isFacingRight = true;
+    public GameObject player;
+    public GameObject player2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jumpCount = jumpExtravalue; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    
+    Jump();
+        moveInput = Input.GetAxisRaw("Horizontal");
+        Vector3 movement = new Vector3(moveInput,0f,0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+        if(moveInput >0 && isFacingRight == false){
+            Flip();
+        }
+        else if (moveInput <0 && isFacingRight == true)
+        {
+            Flip();
+        }
+    }
+    void Jump()
+    {
+        if (isGrounded == true)
+        {
+            jumpCount = jumpExtravalue;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount > 0)
+    { 
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, Jumpfocre), ForceMode2D.Impulse);
+            jumpCount--;
+    }
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount == 0 && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, Jumpfocre), ForceMode2D.Impulse);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.E)) 
+        {
+            player.GetComponent<Move2D>().enabled = false;
+            player2.GetComponent<Move2D>().enabled = true;
+        }
+    }
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+  
+}
